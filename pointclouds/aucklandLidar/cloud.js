3d3fd4f94f5ac80a1ebcb88cfb7fd8f87eeb992d
Wei Ji <weiji@e-spatial.co.nz>
2016-11-17 15:42:30 +1300

    Initial commit showing RGB coloured point clouds for Auckland Tower (tile BA32_3603 only)

---
+++ pointclouds/aucklandLidar/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 4095125,
+    "boundingBox": {
+        "lx": 1756960.0,
+        "ly": 5920080.0,
+        "lz": -37.12,
+        "ux": 1757825.76,
+        "uy": 5920945.76,
+        "uz": 828.64
+    },
+    "tightBoundingBox": {
+        "lx": 1756959.99,
+        "ly": 5920079.99,
+        "lz": -37.13,
+        "ux": 1757440.0,
+        "uy": 5920800.0,
+        "uz": 828.63
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 7.497701644897461,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
